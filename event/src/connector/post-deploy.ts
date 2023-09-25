7eba0c10f47e99b9f633c57d66afc114226995f8
Emiliano <emiliano.mateu@orium.com>
2023-09-25 18:50:48 -0300

    feat: add test cases

--- event/src/connector/post-deploy.ts
+++ event/src/connector/post-deploy.ts
@@ -9,23 +9,20 @@ const CONNECT_GCP_TOPIC_NAME_KEY = 'CONNECT_GCP_TOPIC_NAME';
 const CONNECT_GCP_PROJECT_ID_KEY = 'CONNECT_GCP_PROJECT_ID';
 
 async function postDeploy(properties: Map<string, unknown>): Promise<void> {
-  const topicName = properties.get(CONNECT_GCP_TOPIC_NAME_KEY);
-  const projectId = properties.get(CONNECT_GCP_PROJECT_ID_KEY);
-
-  assertString(topicName, CONNECT_GCP_TOPIC_NAME_KEY);
-  assertString(projectId, CONNECT_GCP_PROJECT_ID_KEY);
+  const topicName = properties.get(CONNECT_GCP_TOPIC_NAME_KEY) as string;
+  const projectId = properties.get(CONNECT_GCP_PROJECT_ID_KEY) as string;
 
   const apiRoot = createApiRoot();
   await createProductPublishedSubscription(apiRoot, topicName, projectId);
 }
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
     const properties = new Map(Object.entries(process.env));
     await postDeploy(properties);
   } catch (error) {
     assertError(error);
-    process.stderr.write(`Post-deploy failed: ${error.message}\n`);
+    process.stderr.write(`Post-deploy failed: ${error.message}`);
     process.exitCode = 1;
   }
 }
