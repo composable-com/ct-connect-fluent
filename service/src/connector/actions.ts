3b048e9cad020c1cc9af15e6ef354c831178f618
Danny Lake <dannytlake@gmail.com>
2023-09-29 13:54:55 -0700

    feat: unit tests for service

--- service/src/connector/actions.ts
+++ service/src/connector/actions.ts
@@ -1,7 +1,7 @@
 import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 
-const CART_UPDATE_EXTENSION_KEY = 'myconnector-cartUpdateExtension';
-const CART_DISCOUNT_TYPE_KEY = 'myconnector-cartDiscountType';
+export const CART_UPDATE_EXTENSION_KEY = 'myconnector-cartUpdateExtension';
+export const CART_DISCOUNT_TYPE_KEY = 'myconnector-cartDiscountType';
 
 export async function createCartUpdateExtension(
   apiRoot: ByProjectKeyRequestBuilder,
